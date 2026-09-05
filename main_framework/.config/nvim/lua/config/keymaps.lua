9a4f39f71346c43d4ea0e70384663384388d7834
Bryan Luu <loc.bryan.luu@gmail.com>
2026-09-04 17:13:31 -0700

    :sparkles: Rebind Ctrl-/ to toggle comments
    Free Ctrl-/ and its <C-/> terminal-encoded twin from
    LazyVim's terminal-toggle defaults and point both at the
    comment operators: gcc for the current line, gc over a
    visual selection.

--- main_framework/.config/nvim/lua/config/keymaps.lua
+++ main_framework/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- ============================================================
+-- Comment toggle: free Ctrl+/ (and its <C-_> terminal-encoded
+-- twin) from LazyVim's default terminal toggle, repurpose for
+-- comments instead
+-- ============================================================
+vim.keymap.del("n", "<C-/>")
+vim.keymap.del("n", "<C-_>")
+
+vim.keymap.set("n", "<C-/>", "gcc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("n", "<C-_>", "gcc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("v", "<C-/>", "gc", { desc = "Toggle comment (selection)", remap = true })
+vim.keymap.set("v", "<C-_>", "gc", { desc = "Toggle comment (selection)", remap = true })
